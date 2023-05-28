51dd91d7c47d84291ba4bb53bba6d9a791f50870
Yusuf Filiz <y.filiz.ch@gmail.com>
2023-05-28 20:09:01 +0200

    generated a header component

---
+++ up.bat
@@ -0,0 +1,7 @@
+@REM to upload into repositories, before that, the current version is updated with pull - just write "./up.bat -commit text- into the Terminal
+
+
+git pull @REM git pull, wenn du in einem Projekt mit mehreren Leuten arbeitest, wenn du alleine arbeitest, git pull einfach löschen
+git add .
+git commit -m "%*"
+git push

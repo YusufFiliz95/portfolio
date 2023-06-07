9e3f31d5ec362657d76cb3619cae15e818a6856e
Yusuf Filiz <y.filiz.ch@gmail.com>
2023-06-07 15:54:25 +0200

    fixed some bugs

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -11,6 +11,5 @@ export class PortfolioComponent {
   projects: Project[] = [
     new Project(1, 'Join', ['Angular', 'Typescript', 'HTML', 'CSS', 'Firebase'], 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.', 'assets/img/project1.png', 'https://github.com/project1', 'https://livesite.com/project1'),
     new Project(2, 'Project 2', ['Tech', 'Stack'], 'Description 2', 'assets/img/project2.png', 'https://github.com/project2', 'https://livesite.com/project2'),
-    // Weitere Projekte hier hinzufügen
   ];
 }

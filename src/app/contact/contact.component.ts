a4bf6f0c9afbd10db257723b96cb8a4842f2fddc
Yusuf Filiz <y.filiz.ch@gmail.com>
2023-06-11 16:16:00 +0200

    now the label on the textarea goes up after klicking on it

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss'],
 })
-export class ContactComponent {
+export class ContactComponent implements AfterViewInit {
   isFocused = false;
+  @ViewChild('textarea') textarea!: ElementRef;
+  @ViewChild('label') label!: ElementRef;
+
+  ngAfterViewInit() {
+    // Set initial state of label
+    this.onInput();
+  }
+
+  onInput() {
+    // check if the textarea is empty
+    if (this.isNotEmpty()) {
+      // if it's not empty, add the 'focused' class to the label
+      this.label.nativeElement.classList.add('focused');
+    } else if (!this.isFocused) {
+      // if it's empty and not focused, remove the 'focused' class from the label
+      this.label.nativeElement.classList.remove('focused');
+    }
+  }
+
+  isNotEmpty() {
+    return this.textarea.nativeElement.value !== '';
+  }
 
   focus(focused: boolean) {
     this.isFocused = focused;
+    this.onInput();
   }
 }

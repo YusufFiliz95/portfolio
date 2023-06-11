8e5f44c4ed92d2bdf001dafa58dac367445a58f7
Yusuf Filiz <y.filiz.ch@gmail.com>
2023-06-11 16:05:37 +0200

    made some changes with the textarea

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
+  isFocused = false;
 
+  focus(focused: boolean) {
+    this.isFocused = focused;
+  }
 }

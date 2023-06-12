4b0e4ded4a820502d79d28b688fc58203bff9ebf
Yusuf Filiz <y.filiz.ch@gmail.com>
2023-06-12 15:20:23 +0200

    added animations for the labels on the inputfield if you click on it

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,32 +6,59 @@ import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
   styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements AfterViewInit {
-  isFocused = false;
+  isNameInputFocused = false;
+  isEmailInputFocused = false;
+  isTextAreaFocused = false;
+  @ViewChild('nameInput') nameInput!: ElementRef;
+  @ViewChild('nameLabel') nameLabel!: ElementRef;
+  @ViewChild('emailInput') emailInput!: ElementRef;
+  @ViewChild('emailLabel') emailLabel!: ElementRef;
   @ViewChild('textarea') textarea!: ElementRef;
-  @ViewChild('label') label!: ElementRef;
+  @ViewChild('textLabel') textLabel!: ElementRef;
 
   ngAfterViewInit() {
     // Set initial state of label
-    this.onInput();
+    this.onTextAreaInput();
+    this.onEmailInput();
   }
 
-  onInput() {
+  onEmailInput() {
     // check if the textarea is empty
-    if (this.isNotEmpty()) {
+    if (this.isEmailInputNotEmpty()) {
       // if it's not empty, add the 'focused' class to the label
-      this.label.nativeElement.classList.add('focused');
-    } else if (!this.isFocused) {
+      this.emailLabel.nativeElement.classList.add('email-focused');
+    } else if (!this.isEmailInputFocused) {
       // if it's empty and not focused, remove the 'focused' class from the label
-      this.label.nativeElement.classList.remove('focused');
+      this.emailLabel.nativeElement.classList.remove('email-focused');
     }
   }
 
-  isNotEmpty() {
+  isEmailInputNotEmpty() {
+    return this.emailInput.nativeElement.value !== '';
+  }
+
+  focusEmailInput(focused: boolean) {
+    this.isEmailInputFocused = focused;
+    this.onEmailInput();
+  }
+
+  onTextAreaInput() {
+    // check if the textarea is empty
+    if (this.isTextAreaNotEmpty()) {
+      // if it's not empty, add the 'focused' class to the label
+      this.textLabel.nativeElement.classList.add('focused');
+    } else if (!this.isTextAreaFocused) {
+      // if it's empty and not focused, remove the 'focused' class from the label
+      this.textLabel.nativeElement.classList.remove('focused');
+    }
+  }
+
+  isTextAreaNotEmpty() {
     return this.textarea.nativeElement.value !== '';
   }
 
-  focus(focused: boolean) {
-    this.isFocused = focused;
-    this.onInput();
+  focusTextArea(focused: boolean) {
+    this.isTextAreaFocused = focused;
+    this.onTextAreaInput();
   }
 }

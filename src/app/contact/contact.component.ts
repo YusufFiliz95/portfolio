6e3a19584bcd6e295d4db06764dcd682624c6b45
Yusuf Filiz <y.filiz.ch@gmail.com>
2023-06-12 21:28:45 +0200

    fixed some animation bugs and added some new features

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, AfterViewInit} from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -6,59 +6,93 @@ import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
   styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements AfterViewInit {
-  isNameInputFocused = false;
-  isEmailInputFocused = false;
-  isTextAreaFocused = false;
-  @ViewChild('nameInput') nameInput!: ElementRef;
-  @ViewChild('nameLabel') nameLabel!: ElementRef;
-  @ViewChild('emailInput') emailInput!: ElementRef;
-  @ViewChild('emailLabel') emailLabel!: ElementRef;
-  @ViewChild('textarea') textarea!: ElementRef;
-  @ViewChild('textLabel') textLabel!: ElementRef;
 
   ngAfterViewInit() {
-    // Set initial state of label
-    this.onTextAreaInput();
-    this.onEmailInput();
   }
 
-  onEmailInput() {
-    // check if the textarea is empty
-    if (this.isEmailInputNotEmpty()) {
-      // if it's not empty, add the 'focused' class to the label
-      this.emailLabel.nativeElement.classList.add('email-focused');
-    } else if (!this.isEmailInputFocused) {
-      // if it's empty and not focused, remove the 'focused' class from the label
-      this.emailLabel.nativeElement.classList.remove('email-focused');
+  updateFormFilledStatus(event: any) {
+    let inputElement = (<HTMLInputElement>event.target);
+
+    if (inputElement.value) {
+      inputElement.classList.add('form-filled');
+    } else {
+      inputElement.classList.remove('form-filled');
     }
   }
 
-  isEmailInputNotEmpty() {
-    return this.emailInput.nativeElement.value !== '';
-  }
+  nameLabelUp() {
+    let nameInput = (<HTMLInputElement>document.getElementById('nameInput'));
+    let nameLabel = document.getElementById('nameLabel');
 
-  focusEmailInput(focused: boolean) {
-    this.isEmailInputFocused = focused;
-    this.onEmailInput();
-  }
+    nameLabel?.classList.add('input-name-label-focused');
 
-  onTextAreaInput() {
-    // check if the textarea is empty
-    if (this.isTextAreaNotEmpty()) {
-      // if it's not empty, add the 'focused' class to the label
-      this.textLabel.nativeElement.classList.add('focused');
-    } else if (!this.isTextAreaFocused) {
-      // if it's empty and not focused, remove the 'focused' class from the label
-      this.textLabel.nativeElement.classList.remove('focused');
+    if (nameInput.value) {
+      nameInput.classList.add('form-filled');
     }
   }
 
-  isTextAreaNotEmpty() {
-    return this.textarea.nativeElement.value !== '';
+  nameLabelDown() {
+    let nameInput = (<HTMLInputElement>document.getElementById('nameInput'));
+    let nameLabel = document.getElementById('nameLabel');
+
+    if (!nameInput.value) {
+      setTimeout(() => {
+        nameLabel?.classList.remove('input-name-label-focused');
+        nameInput.classList.remove('form-filled');
+      }, 40);
+    } else {
+      nameInput.classList.add('form-filled');
+    }
   }
 
-  focusTextArea(focused: boolean) {
-    this.isTextAreaFocused = focused;
-    this.onTextAreaInput();
+
+  emailLabelUp() {
+    let emailInput = (<HTMLInputElement>document.getElementById('emailInput'));
+    let emailLabel = document.getElementById('emailLabel');
+
+    emailLabel?.classList.add('input-email-label-focused');
+
+    if (emailInput.value) {
+      emailInput.classList.add('form-filled');
+    }
+  }
+
+  emailLabelDown() {
+    let emailInput = (<HTMLInputElement>document.getElementById('emailInput'));
+    let emailLabel = document.getElementById('emailLabel');
+
+    if (!emailInput.value) {
+      setTimeout(() => {
+        emailLabel?.classList.remove('input-email-label-focused');
+        emailInput.classList.remove('form-filled');
+      }, 40);
+    } else {
+      emailInput.classList.add('form-filled');
+    }
+  }
+
+  textLabelUp() {
+    let textarea = (<HTMLTextAreaElement>document.getElementById('textArea'));
+    let textLabel = document.getElementById('textLabel');
+
+    textLabel?.classList.add('textarea-label-focused');
+
+    if (textarea.value) {
+      textarea.classList.add('form-filled');
+    }
+  }
+
+  textLabelDown() {
+    let textarea = (<HTMLTextAreaElement>document.getElementById('textArea'));
+    let textLabel = document.getElementById('textLabel');
+
+    if (!textarea.value) {
+      setTimeout(() => {
+        textLabel?.classList.remove('textarea-label-focused');
+        textarea.classList.remove('form-filled');
+      }, 40);
+    } else {
+      textarea.classList.add('form-filled');
+    }
   }
 }

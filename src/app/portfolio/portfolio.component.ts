d30ceb356ffd9c7787400cf0a9a3d7aec756cecb
Yusuf Filiz <y.filiz.ch@gmail.com>
2023-06-13 15:26:33 +0200

    added a legal notice DIV

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -15,8 +15,8 @@ export class PortfolioComponent {
       ['Angular', 'TypeScript', 'HTML', 'CSS', 'Firebase'],
       'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
       'assets/img/Join.png',
-      'https://github.com',
-      'https://github.com'
+      'https://github.com/YusufFiliz95/Gruppenprojekt-Join',
+      'https://yusuf-filiz.developerakademie.net/Join/index.html'
     ),
     new Project(
       2,
@@ -24,8 +24,8 @@ export class PortfolioComponent {
       ['JavaScript', 'HTML', 'CSS'],
       'A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to find coins and salsa bottles to fight aginst El Pollo Loco',
       'assets/img/El-Pollo-Loco.png',
-      'https://github.com',
-      'https://github.com'
+      'https://github.com/YusufFiliz95/Project-Game-El-Pollo-Loco',
+      'https://yusuf-filiz.developerakademie.net/Projekt%20El%20Pollo%20Loco/index.html'
     ),
     new Project(
       3,
@@ -33,8 +33,8 @@ export class PortfolioComponent {
       ['Angular', 'HTML', 'SCSS', 'Firebase'],
       'A card-based party game involving drink-related rules for each card drawn.',
       'assets/img/Ring-Of-Fire.jpg',
-      'https://github.com',
-      'https://github.com'
+      'https://github.com/YusufFiliz95/Game-ringgoffire',
+      'https://y-f-ringoffire.web.app/'
     ),
     new Project(
       3,
@@ -42,8 +42,8 @@ export class PortfolioComponent {
       ['JavaScript', 'HTML', 'CSS', 'API'],
       'a digital encyclopedia application developed with a REST API, providing detailed data on Pokémon characters.',
       'assets/img/Pokedex.png',
-      'https://github.com',
-      'https://github.com'
+      'https://github.com/YusufFiliz95/Project-Pokedex',
+      'https://yusuf-filiz.developerakademie.net/Pokedex/index.html'
     ),
   ];
 

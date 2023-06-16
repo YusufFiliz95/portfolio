04ca5ecff92a1948dbc6c53a2da437aa42e42c8e
Yusuf Filiz <y.filiz.ch@gmail.com>
2023-06-16 13:39:32 +0200

    added some animation for the next html path

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChildren, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
   styleUrls: ['./about-me.component.scss']
 })
-export class AboutMeComponent {
+export class AboutMeComponent implements AfterViewInit {
+  @ViewChildren('animatedElement') animatedElements: ElementRef[] = [];
+
+  ngAfterViewInit() {
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.1
+    }
+
+    let observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if(entry.isIntersecting) {
+          entry.target.classList.add('animate');
+        }
+      });
+    }, options);
+
+    this.animatedElements.forEach(element => {
+      observer.observe(element.nativeElement);
+    });
+  }
+
   scrollToElement(elementId: string): void {
     document.getElementById(elementId)?.scrollIntoView({ behavior: 'smooth' });
   }

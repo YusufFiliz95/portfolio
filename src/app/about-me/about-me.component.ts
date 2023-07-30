c353de0f698adfb966885b45e1b80021d47ebb49
Yusuf Filiz <y.filiz.ch@gmail.com>
2023-07-30 14:58:40 +0200

    Adjusted IntersectionObserver margin to trigger animations earlier

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,52 +1,58 @@
-import { Component, ElementRef, ViewChildren, AfterViewInit, QueryList } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  ViewChildren,
+  AfterViewInit,
+  QueryList,
+} from '@angular/core';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
-  styleUrls: ['./about-me.component.scss']
+  styleUrls: ['./about-me.component.scss'],
 })
 export class AboutMeComponent implements AfterViewInit {
-  @ViewChildren('animatedElement', { read: ElementRef }) animatedElements!: QueryList<ElementRef>;
+  @ViewChildren('animatedElement', { read: ElementRef })
+  animatedElements!: QueryList<ElementRef>;
 
-/**
- * The function initializes the component by subscribing to changes in animated elements and observing
- * those elements.
- */
+  /**
+   * The function initializes the component by subscribing to changes in animated elements and observing
+   * those elements.
+   */
   ngAfterViewInit() {
-    this.animatedElements.changes.subscribe(() => {
-    });
+    this.animatedElements.changes.subscribe(() => {});
 
     this.observeElements();
   }
 
-/**
- * This function observes elements and adds a CSS class to them when they intersect with the viewport.
- */
+  /**
+   * This function observes elements and adds a CSS class to them when they intersect with the viewport.
+   */
   observeElements() {
     const options = {
       root: null,
-      rootMargin: '0px',
-      threshold: 0.1
+      rootMargin: '300px',
+      threshold: 0.1,
     };
 
     let observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
+      entries.forEach((entry) => {
         if (entry.isIntersecting) {
           entry.target.classList.add('animate');
         }
       });
     }, options);
 
-    this.animatedElements.forEach(element => {
+    this.animatedElements.forEach((element) => {
       observer.observe(element.nativeElement);
     });
   }
 
-/**
- * This function scrolls smoothly to a specified element on the page.
- * @param {string} elementId - A string representing the ID of the HTML element that needs to be
- * scrolled into view.
- */
+  /**
+   * This function scrolls smoothly to a specified element on the page.
+   * @param {string} elementId - A string representing the ID of the HTML element that needs to be
+   * scrolled into view.
+   */
   scrollToElement(elementId: string): void {
     document.getElementById(elementId)?.scrollIntoView({ behavior: 'smooth' });
   }
